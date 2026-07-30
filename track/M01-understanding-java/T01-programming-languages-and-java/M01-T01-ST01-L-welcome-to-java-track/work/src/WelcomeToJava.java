dbe3acc5ddaa7395a41c8c2bef35bb4cc98ac71d
Hemanth-pixel111 <nhkceo111@gmail.com>
2026-07-30 18:49:48 +0530

    last changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,9 @@ public class WelcomeToJava {
         System.out.println("Welcome to Java Track");
         System.out.println("I know the Progromming logic");
         System.out.println("I am learning how to use the logic using java");
+        System.out.println("I am ready to code");
+        System.out.println("I am ready to start this journey");
+        System.out.println("Lets Strat coding");
+
     }
 }
