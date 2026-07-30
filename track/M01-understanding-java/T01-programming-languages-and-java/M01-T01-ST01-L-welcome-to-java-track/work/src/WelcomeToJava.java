e89d5f48f94f82e9c118b5ad44598cb3922f166a
Hemanth-pixel111 <nhkceo111@gmail.com>
2026-07-30 18:46:41 +0530

    onemoreline added

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
         // Write your code here
         System.out.println("Welcome to Java Track");
         System.out.println("I know the Progromming logic");
+        System.out.println("I am learning how to use the logic using java");
     }
 }
